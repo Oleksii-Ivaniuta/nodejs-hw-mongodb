e0007825f4a22ac64c4543a355869740e7eebed0
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-08-18 17:55:15 +0100

    add refresh

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,6 +5,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   loginUserController,
   logoutUserController,
+  refreshUsersSessionController,
   registerUserController,
 } from '../controllers/auth.js';
 
@@ -24,4 +25,5 @@ router.post(
 
 router.post('/logout', ctrlWrapper(logoutUserController));
 
+router.post('/refresh', ctrlWrapper(refreshUsersSessionController));
 export default router;
